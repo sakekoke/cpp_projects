d1516cfa1be93a1d01bb8f610397b61fd311fb99
sakekoke <sakekoke42@gmail.com>
2020-12-15 21:39:57 +0600

    no f -> -2. 1 f -> -1, else index

---
+++ whiteBelt/find_seconf_f.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+int main() {
+  	std::string str;
+    int count = 0, index;
+
+  	std::cin >> str;
+
+    for (index = 0; index < str.length(); index += 1)
+    {
+        if (str[index] == 'f')
+        {
+            count += 1; 
+        }
+        if (count == 2)
+        {
+            break;
+        }
+    }
+
+    if (count == 0)
+    {
+        std::cout << -2 << std::endl;
+    }
+    else if (count == 1)
+    {
+        std::cout << -1 << std::endl;
+    }
+    else
+    {
+        std::cout << count << std::endl;
+    }
+
+  	return 0;
+}
