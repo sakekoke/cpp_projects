7718f757d0283abe366203be9b3fd4fd48bcac24
sakekoke <sakekoke42@gmail.com>
2020-12-15 18:10:27 +0600

    Initial commit

---
+++ whiteBelt/division.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+  	int a, b;
+
+  	std::cin >> a >> b;
+
+    if (b == 0)
+    {
+        std::cout << "Impossible";
+    }
+    else
+    {
+        std::cout << a / b;
+    }
+    
+  	return 0;
+}
